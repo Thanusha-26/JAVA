7210d623f580fcbb4b8d4341be5531aacd32d3ce
Thanusha-26 <thanusha2765@gmail.com>
2026-02-25 08:28:37 +0530

    Add files via upload

---
+++ DynamicDispatchShapes.java
@@ -0,0 +1,33 @@
+class Shape
+{
+	void draw()
+	{
+		System.out.println("Drawing a Shape");
+	}
+}
+class Circle extends Shape
+{
+	void draw()
+	{
+		super.draw();
+		System.out.println("Drawing a Circle");
+	}
+}
+class Rectangle extends Shape
+{
+	void draw()
+	{
+		System.out.println("Drawing a Rectangle");
+	}
+}
+public class DynamicDispatchShapes
+{
+	public static void main(String[] args)
+	{
+		Shape s;
+		s = new Circle();
+		s.draw();
+		s = new Rectangle();
+		s.draw();
+	}
+}
