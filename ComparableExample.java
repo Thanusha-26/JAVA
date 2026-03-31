bb8b78aee22b7ce529add5b9e6f5213243d8fc3d
Thanusha-26 <thanusha2765@gmail.com>
2026-03-31 20:33:37 +0530

    Add files via upload

---
+++ ComparableExample.java
@@ -0,0 +1,28 @@
+import java.util.*; 
+class Student implements Comparable<Student> 
+{ 
+int id; 
+String name; 
+Student(int id, String name) 
+{ 
+this.id = id; 
+this.name = name; 
+} 
+public int compareTo(Student s) 
+{ 
+return this.id - s.id; 
+} 
+} 
+public class ComparableExample 
+{ 
+public static void main(String[] args) 
+{ 
+ArrayList<Student> list = new ArrayList<>(); 
+list.add(new Student(3,"Ravi")); 
+list.add(new Student(1,"Kiran")); 
+list.add(new Student(2,"Anil"));
+Collections.sort(list); 
+for(Student s : list) 
+System.out.println(s.id + " " + s.name); 
+} 
+} 
